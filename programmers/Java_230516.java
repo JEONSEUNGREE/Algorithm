45f0be531aee94dfefed3e58a6119313c1ff8e4e
JEONSEUNGREE <jws08219@naver.com>
2023-05-17 00:30:02 +0900

    문자열과영단어 2023-05-17

---
+++ programmers/Java_230516.java
@@ -0,0 +1,29 @@
+// 문자열과영단어
+// https://school.programmers.co.kr/learn/courses/30/lessons/81301
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String tmp = s;
+        String[] converter = 
+            new String[] { 
+                "zero",
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven",
+                "eight",
+                "nine"
+            };
+
+        for(int i = 0; i < converter.length; i++){
+            tmp = tmp.replaceAll(converter[i], String.valueOf(i));
+        }
+         answer = Integer.parseInt(tmp);
+
+        return answer;
+    }
+}
