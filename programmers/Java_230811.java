eced1d4c74a5f87f1456a7c5d13223c4ae87a5a5
JEONSEUNGREE <jws08219@naver.com>
2023-08-13 00:30:02 +0900

    내적 2023-08-13

---
+++ programmers/Java_230811.java
@@ -0,0 +1,16 @@
+// 내적
+// https://school.programmers.co.kr/learn/courses/30/lessons/70128
+
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for(int i = 0; i < a.length; i++){
+            answer += a[i] * b[i];
+        }
+
+        return answer;
+    }
+}
+
+
